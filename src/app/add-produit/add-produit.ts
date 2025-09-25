346a1cdea94a910b02dafa7418fe5d77f6243698
laouitiyomna7 <laouitiyomna7@gmail.com>
2025-09-25 21:40:15 +0100

    first commit

---
+++ src/app/add-produit/add-produit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  imports: [],
+  templateUrl: './add-produit.html',
+ 
+})
+export class AddProduit {
+
+}
